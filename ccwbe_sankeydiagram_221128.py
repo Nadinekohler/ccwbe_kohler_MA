29ec20fd660b2748a16cf02ea1a4c024eb0fd7ec
Nadinekohler <97158632+Nadinekohler@users.noreply.github.com>
2022-12-06 09:41:12 +0100

    sankey diagram

--- ccwbe_sankeydiagram_221128.py
+++ ccwbe_sankeydiagram_221128.py
@@ -8,53 +8,69 @@
 ## Eine partizipative Modellierung von Berner Waldstandorttypen für die Periode 2070-99
 
 
-# Environment settings
-myworkspace="E:/Masterarbeit/Chord_diagrams/1_Durchlauf"
-mycodespace="E:/Masterarbeit/ccwbe_kohler_MA/ccwbe_kohler_MA"
-
 # Modules
 import pandas as pd
 from pySankey.sankey import sankey
 import matplotlib.pyplot as plt
-import plotly
-import plotly.graph_objects as go
-import plotly.express as pex
-import os
+#import plotly
+#import plotly.graph_objects as go
+#import plotly.express as pex
+#import os
 
+
+# Environment settings
+myworkspace="E:/Masterarbeit/Chord_diagrams/1_Durchlauf"
+mycodespace="E:/Masterarbeit/ccwbe_kohler_MA/ccwbe_kohler_MA"
 
 # Read Excel
 print("read data")
 #data = pd.read_csv(myworkspace+"/"+"sttypenintrsct_Jura_HS_heute85.csv",low_memory=False)
-dfdata = pd.read_csv(myworkspace+"/"+"Test_direkt_jura.csv", dtype=str)
+dfdata = pd.read_csv(myworkspace+"/"+"Test_direkt_jura.csv", sep=';')
 dfdata.columns
 dfdata.head(5)
 
-
-data=pd.DataFrame(dfdata, columns=['BE','BE_rcp45','BE_rcp85','flaeche','Code_heute','Code_rcp45','Code_rcp85'],dtype=str)
+# Define important columns
+data=pd.DataFrame(dfdata, columns=['BE','BE_rcp45','BE_rcp85','flaeche','Code_heute','Code_rcp45','Code_rcp85'])
 print(data.head(5))
 data.columns
+data.dtypes
+
+# Make subset with vegetation belt
+sm_data=data[data.Code_heute == 4]
+sm_data.head(5)
+print(sm_data)
+
+# sort descending for the future scenario --> How can I sort within sm_data and not making a new list?
+#sm_data_sorted=sm_data.Code_rcp45.sort_values(ascending=False)
+#print(sm_data_sorted)
+
+# Define group colors for vegetation belt --> doesn't work
+#colors = {
+#    for 2 in Code_rcp45: "#f71b1b",
+#   for 4 in Code_rcp45: "#1b7ef7",
+#}
+
+#hs_all=sm_data.Code_heute.tolist() + sm_data.Code_rcp45.tolist() + sm_data.Code_rcp85.tolist()
+#hs_rcp45=sm_data.Code_rcp45.tolist()
+#print(hs_rcp45)
 
 
-# Draw Sankey Diagram
+# Draw Basic Sankey Diagram
 print("define source, target and area")
-sankey(left=data["BE"], right=data["BE_rcp85"], leftWeight=data["flaeche"], rightWeight=data["flaeche"], aspect=20, fontsize=9)
-#sankey(left=data["hsheute"], right=data["hszukunft"], leftWeight=data["BE"], rightWeight=data["BE_zukunft"], aspect=20, fontsize=9)
-#sankey(left=data[source_indices], right=data[target_indices], leftWeight=data[source_flaeche], rightWeight=data[target_flaeche], aspect=20, fontsize=9)
+sankey(left=sm_data["BE"], right=sm_data["BE_rcp85"], leftWeight=sm_data["flaeche"], rightWeight=sm_data["flaeche"], figure_name=["Test"], aspect=4, fontsize=9)
 
-# Get current figure
-print("show sankey diagram")
-fig = plt.gcf()
 
-# Format diagram
-print("adjust format")
-# Set size
-fig.set_size_inches(6, 6)
 
-# Set background to white
-fig.set_facecolor("w")
-
-fig.update_layout(title_text="Veränderung der Standorttypen", font_size=10)
-
-# Save the figure
-print("export diagram")
-fig.savefig(myworkspace+"test.png", bbox_inches="tight", dpi=150)
+# Save the figure --> doesn't work
+#print("export diagram")
+#fig.savefig(myworkspace+"test.png", bbox_inches="tight", dpi=150)
+## Get current figure
+#print("show sankey diagram")
+#fig = plt.gcf()
+## Format diagram
+#print("adjust format")
+## Set size
+#fig.set_size_inches(6, 6)
+## Set background to white
+#fig.set_facecolor("w")
+#fig.update_layout(title_text="Veränderung der Standorttypen", font_size=10)
--- ccwbe_sankeydiagram_221129.py
+++ ccwbe_sankeydiagram_221129.py
@@ -9,12 +9,11 @@
 
 
 # Environment settings
-import plotly.io
-
 myworkspace="E:/Masterarbeit/Chord_diagrams/1_Durchlauf"
 mycodespace="E:/Masterarbeit/ccwbe_kohler_MA/ccwbe_kohler_MA"
 
 # Modules
+import plotly.io
 import pandas as pd
 from pySankey.sankey import sankey
 import matplotlib.pyplot as plt
@@ -27,23 +26,30 @@ import os
 
 # Read Excel
 print("read data")
-data = pd.read_csv(myworkspace+"/"+"sttypenintrsct_Jura_HS_heute85.csv", low_memory=False)
-data.columns
+dfdata = pd.read_csv(myworkspace+"/"+"Test_direkt_jura.csv", sep=";", low_memory=False)
+dfdata.columns
+dfdata.head(5)
 
+data=pd.DataFrame(dfdata, columns=['BE','BE_rcp45','BE_rcp85','flaeche','Code_heute','Code_rcp45','Code_rcp85'])
+data.head(5)
 
 # Define all chart nodes
-all_nodes=data.BE.values.tolist() + data.BE_zukunft.values.tolist()
+all_nodes=data.BE.values.tolist() + data.BE_rcp45.values.tolist() + data.BE_rcp85.values.tolist()
+all_nodes
 
-# Indices of sources and destinations
+#Define vegetation belts
+all_hs=data.Code_heute.tolist() + data.Code_rcp45.tolist() + data.Code_rcp85.tolist()
+
+# Indices of sources and targets
 source_indices = [all_nodes.index(BE) for BE in data.BE]
-target_indices = [all_nodes.index(BE_zukunft) for BE_zukunft in data.BE_zukunft]
+target_indices = [all_nodes.index(BE_rcp45) for BE_rcp45 in data.BE_rcp45]
 
 
 fig = go.Figure(data=[go.Sankey(
                         # Define nodes
                         node = dict(
-                          label =  all_nodes,
-                          color =  "black"
+                          label = all_hs,
+                          color = (for 2 in all_hs["black"]), (for 4 in all_hs["orange"]), (for 5 in all_hs["purple"]),(for 6 in all_hs["yellow"]),(for 8 in all_hs["blue"]),(for 9 in all_hs["green"]),(for 10 in all_hs["red"]),
                         ),
 
                         # Add links
@@ -57,9 +63,6 @@ fig = go.Figure(data=[go.Sankey(
 
 fig.update_layout(title_text="test",
                   font_size=10)
-
-fig
-
 
 
 plotly.io.write_image(fig,myworkspace + "test.jpeg")
